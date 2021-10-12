2426aef047f50964c21529131ee817e618761a09
Lucas Teske <lucas@teske.net.br>
2021-10-12 04:31:13 -0300

    Works

---
+++ README.md
@@ -0,0 +1,30 @@
+# RISC-V Online Assembler
+
+This is a very crude online assembler for RISC-V assembly (all variants that gas supports)
+
+It uses a webassembly compiled version of gnu as, objdump and objcopy to build the assembly.
+
+It has been done REALLY quick for my RISC-V Emulator series (currently only in portuguese at [https://www.youtube.com/playlist?list=PLEP_M2UAh9q6_2Jtvs9fgOVlRgsruii2m](https://www.youtube.com/playlist?list=PLEP_M2UAh9q6_2Jtvs9fgOVlRgsruii2m))
+
+### Compiling binutils
+
+```bash
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.31.tar.xz
+tar -xf binutils-2.31.tar.xz
+rm binutils-2.31.tar.xz
+mkdir -p build
+mkdir -p bins
+mkdir -p web
+cd build
+source {PATH_TO_EMSDK}/emsdk_env.sh
+echo "Module['FS'] = FS;" > post-js.txt
+emconfigure ../binutils-2.31/configure --disable-doc --build=x86 --host=wasm32 --target=riscv64-linux-gnu
+emmake make -j4 CFLAGS="-DHAVE_PSIGNAL=1 -DELIDE_CODE -D__GNU_LIBRARY__ -O2" LDFLAGS="-s MODULARIZE=1 -s FORCE_FILESYSTEM=1 --post-js $(pwd)/post-js.txt"
+emmake make install DESTDIR="$(pwd)/../bins"
+cp binutils/objcopy.wasm binutils/objdump.wasm gas/as-new.wasm ../web
+cd ..
+cd bins
+cp usr/local/bin/riscv64-linux-gnu-as ../web/riscv64-linux-gnu-as.js
+cp usr/local/bin/riscv64-linux-gnu-objcopy ../web/riscv64-linux-gnu-objcopy.js
+cp usr/local/bin/riscv64-linux-gnu-objdump ../web/riscv64-linux-gnu-objdump.js
+```
---
+++ js/main.js
@@ -0,0 +1,161 @@
+async function callAS(env) {
+  return new Promise((resolve) => {
+    require(['./riscv64-linux-gnu-as'], (Module) => {
+      resolve(Module(env));
+    });
+  })
+}
+async function callObjdump(env) {
+  return new Promise((resolve) => {
+    require(['./riscv64-linux-gnu-objdump'], (Module) => {
+      resolve(Module(env));
+    });
+  })
+}
+async function callObjcopy(env) {
+  return new Promise((resolve) => {
+    require(['./riscv64-linux-gnu-objcopy'], (Module) => {
+      resolve(Module(env));
+    });
+  })
+}
+
+async function assemble(code) {
+  return new Promise((resolve, reject) => {
+    const env = {}
+    env.preRun = [(m) => m.FS.writeFile("file.s", code)];
+    let out = "";
+    env.print = (data) => {
+        out += data + "\n";
+    }
+    env.printErr = (data) => {
+      out += data + "\n";
+    }
+    let error = false;
+    env.quit = (code, err)=>{
+      if (env.quit && code !== 0) {
+        error = true;
+      }
+      env.quit = null;
+    };
+    env.postRun = [
+      async (m) => {
+        if (!error) {
+          const data = await m.FS.readFile("file.elf")
+          resolve(data)
+        } else {
+          reject(out);
+        }
+      }
+    ]
+    env.arguments = ["file.s", "-o","file.elf"]
+    callAS(env)
+  })
+}
+
+async function dump(elf) {
+  return new Promise((resolve) => {
+    const env = {}
+    env.preRun = [
+      (m) => {
+        m.FS.writeFile("file.elf", elf)
+      }
+    ];
+    let stdout = "";
+    env.print = (data) => {
+        stdout += data + "\n";
+    }
+    env.postRun = [
+      async (m) => {
+        resolve(stdout)
+      }
+    ]
+    env.arguments = ["-S", "file.elf"]
+    callObjdump(env)
+  })
+}
+
+async function getBinaryText(elf) {
+  return new Promise((resolve) => {
+    const env = {}
+    env.preRun = [
+      (m) => {
+        m.FS.writeFile("file.elf", elf)
+      }
+    ];
+    env.postRun = [
+      async (m) => {
+        const data = await m.FS.readFile("file.bin")
+        resolve(data)
+      }
+    ]
+    env.arguments = ["-O", "binary", "file.elf", "file.bin", "--only-section", ".text*"]
+    callObjcopy(env)
+  })
+}
+
+function selectBinaryBox() {
+  if (document.selection) { // IE
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById('binaryBox'));
+      range.select();
+  } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById('binaryBox'));
+      window.getSelection().removeAllRanges();
+      window.getSelection().addRange(range);
+  }
+}
+
+function bufferToHex(buff) {
+  const dataview = new DataView(buff.buffer);
+  let hexString = '';
+  const hexOcts = (buff.byteLength / 4) >>> 0;
+  for (let i = 0; i < hexOcts; i++) {
+    const v = dataview.getInt32(i*4, true) >>> 0;
+    hexString += v.toString(16).padStart(8, '0') + "\n"
+  }
+  return hexString;
+}
+
+function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
+
+async function doAssemble(code) {
+  const elf = await assemble(code);
+  const data = await dump(elf);
+  const bin = await getBinaryText(elf);
+
+  return {
+    elf,
+    data,
+    bin: _arrayBufferToBase64(bin),
+    hex: bufferToHex(bin)
+  }
+}
+
+async function buildStuff() {
+  try {
+    const code = document.getElementById('sourceCodeBox').value;
+    const l = await doAssemble(code);
+    document.getElementById('binaryBox').innerHTML = l.hex;
+    document.getElementById('objDumpBox').innerHTML = l.data;
+    document.getElementById('output').innerHTML = '<span style="color: green">OK!</span>';
+  } catch(e) {
+    document.getElementById('output').innerHTML = `<span style="color: red">${e}</span>`;
+  }
+}
+
+async function main(require) {
+  window.require = require;
+  window.doAssemble = doAssemble;
+}
+
+define(main)
